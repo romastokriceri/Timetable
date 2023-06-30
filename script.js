0b7d22183398d0e0e71f5d9fd11e8dbdd9951d93
romastokriceri <75224493+romastokriceri@users.noreply.github.com>
2023-06-30 16:05:59 +0300

    add more fitures with js
    color change depends on day of the week

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 
 // Функція для оновлення значення поля
 function updateWeeklyValue() {
-    var currentDate = new Date(); // Поточна дата та час
-  var septemberFirst = new Date(currentDate.getFullYear(), 8, 1); // 1 вересня поточного року
-  var weekNumber = Math.floor((currentDate - septemberFirst) / (7 * 24 * 60 * 60 * 1000)) + 1; // Номер поточного тижня
- 
-//   var currentDate = moment(); // Поточна дата та час
-//    var septemberFirst = moment().startOf('year').month(8).date(1); // 1 вересня поточного року
-//   var weekNumber = currentDate.diff(septemberFirst, 'weeks'); // Номер поточного тижня
+  var currentDate = moment(); // Поточна дата та час
+   var septemberFirst = moment().startOf('year').month(8).date(1); // 1 вересня поточного року
+  var weekNumber = currentDate.diff(septemberFirst, 'weeks'); // Номер поточного тижня
   var weeklyValueElement = document.querySelector('.weeklyValue'); // Елемент, який потрібно оновити
 
   // Перевіряємо, чи номер тижня парний чи непарний
@@ -24,20 +20,82 @@ updateWeeklyValue();
 // Встановлюємо інтервал для оновлення значення поля кожний тиждень
 setInterval(updateWeeklyValue, 7 * 24 * 60 * 60 * 1000); // Оновлюємо кожні 7 днів
 
-// Функція для відображення поточної дати та часу
-function displayDateTime() {
-  var dateTimeElement = document.querySelector('.date-time');
+ // Функція для виведення часу
+    function displayTime() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
 
-  if (dateTimeElement) {
-    var currentDate = new Date();
-    var dateTimeString = currentDate.toLocaleString(); // Отримуємо рядкове представлення поточної дати та часу
+      hours = formatTimeElement(hours);
+      minutes = formatTimeElement(minutes);
+      seconds = formatTimeElement(seconds);
 
-    dateTimeElement.textContent = dateTimeString;
-  }
+      var timeString = hours + ':' + minutes + ':' + seconds;
+      document.querySelector('.time').textContent = timeString;
+    }
+
+    function formatTimeElement(timeElement) {
+      return timeElement < 10 ? '0' + timeElement : timeElement;
+      
+      // Функція для виведення дати
+    }
+    function displayDate() {
+      var currentDate = new Date();
+      var day = currentDate.getDate();
+      var month = currentDate.getMonth()+1;
+      var year = currentDate.getFullYear();
+
+  // Форматуємо місяць та день у двозначний вигляд, якщо потрібно
+  month = formatTimeElement(month);
+  day = formatTimeElement(day);
+  year =  String(year).slice(-2);
+
+      var dateString = day + ':' + month + ':' + year + ',';
+      document.querySelector('.date').textContent = dateString;
+    }
+    
+   displayTime();
+   displayDate();
+
+    setInterval(displayTime, 1000);
+
+var currentDate = new Date();
+var dayOfWeek = currentDate.getDay(); // Отримуємо день тижня (0 - неділя, 1 - понеділок, і т.д.)
+
+var monday = document.querySelector('.Monday');
+var tuesday = document.querySelector('.Tuesday');
+var wednesday = document.querySelector('.Wednesday');
+var thursday = document.querySelector('.Thursday');
+var friday = document.querySelector('.Friday');
+
+
+// Встановлюємо кольори тексту для кожного дня тижня
+switch (dayOfWeek) {
+  case 1:
+    monday.style.color = 'var(--highlighted-text)';
+    break;
+  case 2:
+    tuesday.style.color = 'var(--highlighted-text)';
+    break;
+  case 3:
+    wednesday.style.color = 'var(--highlighted-text)';
+    break;
+  case 4:
+    thursday.style.color = 'var(--highlighted-text)';
+    break;
+  case 5:
+    friday.style.color = 'var(--highlighted-text)';
+     break;
+case 6:
+   friday.style.color = 'var(--font-title-color)';
+   break;
+default:
+    break;
 }
 
-// Викликати функцію один раз для відображення початкової дати та часу
-displayDateTime();
 
-// Оновлювати дату та час кожну секунду
-setInterval(displayDateTime, 1000);
+// TODO: ЗРОБИ фу-цію, що пише який сьогодні тиждень, переробити ії так, щоб ця фун-ція показувала парний сьогодні тиждень чи ні та вважати тиждень де є 1 вересня та 1 березня непарними за замовчуванням
+
+
+
